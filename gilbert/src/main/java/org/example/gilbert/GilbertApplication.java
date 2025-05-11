0c346f661bccefa63e823cbefc0c737054ee40cf
HenningMand <146338551+cao002eduzealand@users.noreply.github.com>
2025-05-11 12:36:30 +0200

    Add files via upload

---
+++ gilbert/src/main/java/org/example/gilbert/GilbertApplication.java
@@ -0,0 +1,13 @@
+package org.example.gilbert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilbertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilbertApplication.class, args);
+    }
+
+}
---
+++ gilbert/src/test/java/org/example/gilbert/GilbertApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gilbert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilbertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
